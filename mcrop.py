c3f958c3cd2f54f2752aaacd7fce8e38d8ed4192
lygyxr <lyggb721210@163.com>
2025-01-29 10:02:02 +0800

    添加声明文本

--- mcrop.py
+++ mcrop.py
@@ -2,10 +2,17 @@
 这是一个在Python上运行的迷宫挑战游戏
 This is a maze challenge game on Python.
 Email 邮箱：lyggb721210@163.com
-Author 作者：lygyxr / lyggb721210 / yxr
 Version 当前版本：V2.0-pre1
 Please report any bugs to the author.
 如果有BUG请报告给作者。
+
+Copyright (c) 2025 yxrlyg & lyggb721210
+   Mcrop is licensed under Mulan PSL v2.
+   You can use this software according to the terms and conditions of the Mulan PSL v2.
+   You may obtain a copy of Mulan PSL v2 at:
+            http://license.coscl.org.cn/MulanPSL2
+   THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+   See the Mulan PSL v2 for more details.
 """
 
 import json
